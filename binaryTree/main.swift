d1317e8543980b33a231b263e24e3cc37b4dfeef
Tuncay Cansız <32176945+Devinsomnia@users.noreply.github.com>
2017-12-30 03:00:06 +0300

    Initial Commit

---
+++ binaryTree/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  binaryTree
+//
+//  Created by Tuncay Cansız on 30.12.2017.
+//  Copyright © 2017 Tuncay Cansız. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
